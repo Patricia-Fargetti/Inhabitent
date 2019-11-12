015c75b0fff157665995a3a39c335d72b34b998c
Patricia <pat.fargetti@gmail.com>
2019-11-11 22:46:06 -0800

    display header

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -26,19 +26,22 @@
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+				
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+				<div class="header-menu">
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
 			<a href="<?php echo home_url(); ?>">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-tent-white.svg"/>
+				<img class="tent-logo" src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-tent-white.svg"/>
 			</a>
-
+			</div><!--header-menu -->
 		<div class="search_form"><?php get_search_form(); ?> </div>
-
+		<div class="centered-logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg"/>
+</div> <!--centered-logo -->
 
 
 			
