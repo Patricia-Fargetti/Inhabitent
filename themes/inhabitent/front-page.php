be50184d4f96993248d5d970c35c90295dea9797
Patricia <pat.fargetti@gmail.com>
2019-11-15 15:39:11 -0800

    fixing journal items in frontpg

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -46,19 +46,29 @@ get_header(); ?>
         );
         $product_posts = get_posts($args); // returns an array of posts
         ?>
-        <section class="f-journals">
-            <?php
-            foreach ($product_posts as $post) : setup_postdata($post); ?>
-                <div class='f-journal'>
-                    <div class='journal-thumbnail' style='background-image: url(" <?php the_post_thumbnail_url() ?> ");'> </div>
-                    <?php the_post_thumbnail_url('medium');
 
-                        the_title();
-                        comments_number(); ?>
+        <section class="latest-posts">
+            <div class="container">
+                <h2 class="section-name"> Inhabitent Journal</h2>
+                <div class="f-journals">
+                    <?php
+                    foreach ($product_posts as $post) : setup_postdata($post); ?>
+                        <article class='f-journal'>
+                            <div class='journal-thumbnail' style='background-image: url(" <?php the_post_thumbnail_url() ?> ");'> </div>
+
+                            <div class="journal-meta">
+                                <p class="read-entry"><a href="<?php echo get_post_permalink($post); ?>"> Read entry</a></p>
+                                <h3 class='related-post-title'> <?php the_title(); ?></h3>
+                                <div class='comments-post'> <?php comments_number(); ?> </div>
+
+                            </div>
+
+                        </article>
+                    <?php
+                    endforeach;
+                    wp_reset_postdata();  ?>
                 </div>
-            <?php
-            endforeach;
-            wp_reset_postdata();  ?>
+            </div>
         </section>
 
 
