a4d29983cada4e2f9d607db0d982e3de8ca8c16e
Patricia <pat.fargetti@gmail.com>
2019-11-15 12:05:33 -0800

    posts  updates in front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -29,7 +29,7 @@ get_header(); ?>
                 <div class="product-type-blocks">
                     <?php foreach ($terms as $term) : ?>
                         <div class="product-type-block-wrapper">
-                            <img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+                            <img src=<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug . '.svg' ?> alt=<?php echo $term->name; ?> />
                             <p><?php echo $term->description; ?></p>
                             <p><a href="<?php echo get_term_link($term); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
                         </div>
@@ -37,6 +37,31 @@ get_header(); ?>
                 </div>
             <?php endif; ?>
         </section>
+
+        <?php
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => 3,
+            'order' => 'ASC'
+        );
+        $product_posts = get_posts($args); // returns an array of posts
+        ?>
+        <section class="f-journals">
+            <?php
+            foreach ($product_posts as $post) : setup_postdata($post); ?>
+                <div class='f-journal'>
+                    <div class='journal-thumbnail' style='background-image: url(" <?php the_post_thumbnail_url() ?> ");'> </div>
+                    <?php the_post_thumbnail_url('medium');
+
+                        the_title();
+                        comments_number(); ?>
+                </div>
+            <?php
+            endforeach;
+            wp_reset_postdata();  ?>
+        </section>
+
+
 
 
 
