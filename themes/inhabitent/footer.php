a98f5cd0ac15f5861f7a4e40ed83bd413def5cac
Patricia <pat.fargetti@gmail.com>
2019-11-16 00:16:38 -0800

    display items in footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,45 +11,46 @@
 </div><!-- #content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-	<div class="footer-block">
+	<div class="footer-style container">
 		<div class="footer-block-item">
-			<div class="contact-info">
-				<h3> Contact Info </h3>
-				<a href="mailto:info@inhabitent.com"><i class="fas fa-envelope"></i>info@inhabitent.com</a>
-				<a href="tel:778-456-7891"><i class="fas fa-phone"></i>778-456-7891</a>
-				<p>
-					<span> <i class="fab fa-facebook-square"></i> </span>
-					<span><i class="fab fa-twitter-square"></i></span>
-					<span><i class="fab fa-instagram"></i></span>
+			<div class="this">
+				<div class="contact-info">
+					<h3> Contact Info </h3>
+					<a href="mailto:info@inhabitent.com"><i class="fas fa-envelope"></i>info@inhabitent.com</a>
+					<a href="tel:778-456-7891"><i class="fas fa-phone"></i>778-456-7891</a>
+					<p>
+						<span> <i class="fab fa-facebook-square"></i> </span>
+						<span><i class="fab fa-twitter-square"></i></span>
+						<span><i class="fab fa-instagram"></i></span>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="footer-block-item">
-		<div class="Business">
-			<h3> Business Hours </h3>
-			<p>
-				<span class="day-of-week"> Monday-Friday: </span>
-				9am to 5pm
-				<span class="day-of-week">Saturday:</span> 10am to 2 pm
-				<span class="day-of-week"> Sundays:</span> Closed
-				<div class="footer-block-item">
-					<div class="footer-logo">
-						<a href="https://tent.academy.red/">
-							<img src=<?php echo get_template_directory_uri() . "/images/logos/inhabitent-logo-text-dark.svg" ?> />
+		<div class="footer-block-item">
+			<div class="business">
+				<h3> Business Hours </h3>
+				<p>
+					<span class="day-of-week"> Monday-Friday:</span>9am to 5pm
 
-							<!-- <img src=<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug . '.svg' ?> alt=<?php echo $term->name; ?> /> -->
-
-						</a>
-					</div>
-				</div>
+					<span class="day-of-week">Saturday:</span>10am to 2 pm
+					<span class="day-of-week"> Sundays:</span>Closed
+			</div>
 		</div>
-	</div>
-	</div><!-- .site-info -->
-</footer><!-- #colophon -->
-</div><!-- #page -->
+		<div class="footer-block-item-img">
+			<div class="footer-logo">
 
-<?php wp_footer(); ?>
+				<img src=<?php echo get_template_directory_uri() . "/images/logos/inhabitent-logo-text.svg" ?> />
+			</div>
+		</div>
+		<div class="container">
+			<p class="copyright"> COPYRIGHT © 2019 INHABITENT</p>
+		</div>
+	</div> <!-- footer-style -->
+	</div><!-- .site-info -->
+	</div><!-- #page -->
+</footer><!-- #colophon -->
+
 
 </body>
+<?php wp_footer(); ?>
 
 </html>
