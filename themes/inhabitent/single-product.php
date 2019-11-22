f0edbd3e4c50d906d0318274afc7eb1e2f18fda5
Patricia <pat.fargetti@gmail.com>
2019-11-22 15:57:23 -0800

    single journal page

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 		<?php while (have_posts()) : the_post(); ?>
 			<h2> Price: $<?php the_field('price'); ?></h2>
-			<?php get_template_part('template-parts/content', 'single'); ?>
+			<?php get_template_part('template-parts/content', 'product'); ?>
 
 			<?php the_post_navigation(); ?>
 
